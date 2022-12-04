554446b86cd2a8427e5a11b4e438b7786372a724
john.li <john.li@loopring.io>
2022-12-04 16:36:27 +0800

    guardian wip

--- packages/webapp/src/pages/WalletPage/index.tsx
+++ packages/webapp/src/pages/WalletPage/index.tsx
@@ -16,6 +16,7 @@ import {
 } from "@loopring-web/common-resources";
 import { Box, Button, Link, Typography } from "@mui/material";
 import {
+  AModal,
   GuardianStep,
   ModalQRCode,
   SubMenu,
@@ -31,6 +32,7 @@ import { WalletValidationInfo } from "./WalletValidationInfo";
 import { WalletProtector } from "./WalletProtector";
 import { useTheme } from "@emotion/react";
 import styled from "@emotion/styled";
+import { Protector } from "@loopring-web/loopring-sdk";
 
 const WrongStatusStyled = styled(Box)`
   display: flex;
@@ -72,10 +74,13 @@ const WrongStatus = ({ logo, content, onClickDisconnect }: { logo: string, conte
 }
 
 const SectionStyled = styled(Box)`
-  padding: auto ${({theme}) => theme.unit * 4}px;
+  padding: ${({theme}) => theme.unit * 4}px;
+  padding-top: auto;
+  padding-bottom: auto;
   background: ${({theme}) => theme.colorBase.box};
   margin-bottom: ${({theme}) => theme.unit * 2}px;
   width: ${({theme}) => theme.unit * 60}px;
+  height: ${({theme}) => theme.unit * 12}px;
   cursor: pointer;
   display: flex;
   justify-content: space-between;
@@ -91,10 +96,44 @@ const Section = ({logo, title, description, onClick} : {logo: JSX.Element, title
           <Typography variant={"h4"}>{title}</Typography> 
           {description && <Typography color={"var(--color-text-third) "}>{description}</Typography>}
         </Box>
-        
       </Box>
       <RightIcon />
     </SectionStyled>
+  </>
+}
+
+// const WalletProtectorStyled = styled(Box)`
+//   display: flex; 
+//   justify-content: space-between;
+// `
+// WalletProtectors
+const WalletProtectors = ({ protectorList }: { protectorList: Protector[] }) => {
+  return <>
+    {protectorList.map(x => <>
+      <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"} marginBottom={4}>
+        <Box>
+          {/* todo: Unknown translation */}
+          <Typography variant={"h6"}>{x.ens ? x.ens : 'Unknown'}</Typography> 
+          <Typography color={"var(--color-text-third)"}>{x.address}</Typography>
+        </Box>
+        <Button variant={"outlined"} size={"medium"} color={"secondary"}>Lock Wallet</Button>
+      </Box>
+    </>)}
+  </>
+}
+
+const RqeusetApprovals = ({ protectorList }: { protectorList: Protector[] }) => {
+  return <>
+    {protectorList.map(x => <>
+      <Box display={"flex"} alignItems={"center"} justifyContent={"space-between"} marginBottom={4}>
+        <Box>
+          {/* todo: Unknown translation */}
+          <Typography variant={"h6"}>{x.ens ? x.ens : 'Unknown'}</Typography> 
+          <Typography color={"var(--color-text-third)"}>{x.address}</Typography>
+        </Box>
+        <Button variant={"outlined"} size={"medium"} color={"secondary"}>Lock Wallet</Button>
+      </Box>
+    </>)}
   </>
 }
 
@@ -124,10 +163,10 @@ const YoStyled = styled(Box)`
 
 const Yo = () => {
   return <YoStyled marginTop={2}>
-    <Section onClick={() => {}} title={"Set as Guardian"} logo={<RoundAddIcon style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}}  />} />
-    <Section description={"Who I Protect"} onClick={() => {}} title={"Lock/unlock Wallet"}  logo={<LockIcon2 style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}}/>} />
-    <Section description={"Guardian Request Handling"} onClick={() => {}} title={"Approve Requests"} logo={<ApprovalIcon style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}} />} />
-    <Section description={"Guardian Handling Records"} onClick={() => {}} title={"View History"} logo={<ViewHistoryIcon style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}} />} />
+    <Section onClick={() => {}} title={"Set as Guardian"} logo={<RoundAddIcon htmlColor="var(--color-text-primary)" style={{ width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}}  />} />
+    <Section description={"Who I Protect"} onClick={() => {}} title={"Lock/unlock Wallet"}  logo={<LockIcon2 htmlColor="var(--color-text-primary)" style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}}/>} />
+    <Section description={"Guardian Request Handling"} onClick={() => {}} title={"Approve Requests"} logo={<ApprovalIcon htmlColor="var(--color-text-primary)" style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}} />} />
+    <Section description={"Guardian Handling Records"} onClick={() => {}} title={"View History"} logo={<ViewHistoryIcon htmlColor="var(--color-text-primary)" style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}} />} />
   </YoStyled>
 }
 
@@ -136,9 +175,27 @@ export const GuardianPage = withTranslation(["common"])(
     const { account } = useAccount();
     let match = useRouteMatch("/guardian/:item");
     const [openQRCode, setOpenQRCode] = React.useState(false);
-    const onOpenAdd = React.useCallback(() => {
-      setOpenQRCode(true);
+    const onOpenAdd = React.useCallback((open :boolean) => {
+      setOpenQRCode(open);
     }, []);
+    const [showHistory, setShowHistory] = React.useState(false);
+    const onOpenHistory = React.useCallback((open :boolean) => {
+      setShowHistory(open);
+    }, []);
+    const [showApprovalRequests, setShowApprovalRequests] = React.useState(false);
+    const onOpenApprovalRequests = React.useCallback((open :boolean) => {
+      setShowApprovalRequests(open);
+    }, []);
+
+    const [showLockWallet, setShowLockWallet] = React.useState(false);
+    const onOpenLockWallet = React.useCallback((open :boolean) => {
+      setShowLockWallet(open);
+    }, []);
+    
+    // const openQRCode = match?.params['item'] === 'add-guardian';
+    // if (match?.params?['item'] === 'add-guardian') {
+    //   openQRCode
+    // }
     const description = () => (
       <Typography
         marginTop={2}
@@ -168,7 +225,17 @@ export const GuardianPage = withTranslation(["common"])(
       isLoading,
       isContractAddress,
     } = useHebaoMain();
-
+    // address
+// : 
+// "0x2bbb00232bead228973528dc7590fcb366bb9856"
+// ens
+// : 
+// "testtest.loopring.eth"
+// lockStatus
+// : 
+// "LOCKED"
+// protectList[0].
+    console.log('protectList', protectList)
     const handleOpenModal = ({
       step,
       options,
@@ -395,7 +462,119 @@ export const GuardianPage = withTranslation(["common"])(
       isLoading,
       guardianRouter,
     ]);
-    return <Yo/>
+    
+    return <>
+      <ModalQRCode
+        open={openQRCode}
+        fgColor={"#000"}
+        bgColor={"#fff"}
+        className={"guardianPop"}
+        onClose={() => onOpenAdd(false)}
+        title={
+          <Typography component={"p"} textAlign={"center"} marginBottom={1}>
+            <Typography
+              color={"var(--color-text-primary)"}
+              component={"p"}
+              variant={"h4"}
+              marginBottom={2}
+            >
+              {t("labelWalletAddAsGuardian")}
+            </Typography>
+            <Typography
+              color={"var(--color-text-secondary)"}
+              component={"p"}
+              variant={"body1"}
+              marginBottom={2}
+            >
+              {t("labelWalletScanQRCode")}
+            </Typography>
+          </Typography>
+        }
+        size={260}
+        description={description()}
+        url={`ethereum:${account?.accAddress}?type=${account?.connectName}&action=HebaoAddGuardian`}
+      />
+      <AModal
+        open={showLockWallet}
+        onClose={() => onOpenLockWallet(false)}
+        title={
+          <Typography component={"p"} textAlign={"center"} marginBottom={1}>
+            <Typography
+              color={"var(--color-text-primary)"}
+              component={"p"}
+              variant={"h4"}
+              marginBottom={2}
+            >
+              Lock/unlock Wallet
+            </Typography>
+            <Typography
+              color={"var(--color-text-secondary)"}
+              component={"p"}
+              variant={"body1"}
+              marginBottom={2}
+            >
+              Who I Protect
+            </Typography>
+          </Typography>
+        }
+        body={<WalletProtectors protectorList={protectList}></WalletProtectors>}
+      />
+      <AModal
+        open={showApprovalRequests}
+        onClose={() => onOpenApprovalRequests(false)}
+        title={
+          <Typography component={"p"} textAlign={"center"} marginBottom={1}>
+            <Typography
+              color={"var(--color-text-primary)"}
+              component={"p"}
+              variant={"h4"}
+              marginBottom={2}
+            >
+              Approval Requests
+            </Typography>
+            <Typography
+              color={"var(--color-text-secondary)"}
+              component={"p"}
+              variant={"body1"}
+              marginBottom={2}
+            >
+              Guardian Request Handling
+            </Typography>
+          </Typography>
+        }
+        body={<RqeusetApprovals protectorList={req}/>}
+      />
+      <ModalQRCode
+        open={showHistory}
+        fgColor={"#000"}
+        bgColor={"#fff"}
+        className={"guardianPop"}
+        onClose={() => onOpenHistory(false)}
+        title={
+          <Typography component={"p"} textAlign={"center"} marginBottom={1}>
+            <Typography
+              color={"var(--color-text-primary)"}
+              component={"p"}
+              variant={"h4"}
+              marginBottom={2}
+            >
+              View History
+            </Typography>
+          </Typography>
+        }
+        size={260}
+        description={description()}
+        url={`ethereum:${account?.accAddress}?type=${account?.connectName}&action=HebaoAddGuardian`}
+      />
+      <YoStyled marginTop={2}>
+        <Section onClick={() => onOpenAdd(true)} title={"Set as Guardian"} logo={<RoundAddIcon htmlColor="var(--color-text-primary)" style={{ width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}}  />} />
+        <Section description={"Who I Protect"} onClick={() => onOpenLockWallet(true)} title={"Lock/unlock Wallet"}  logo={<LockIcon2 htmlColor="var(--color-text-primary)" style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}}/>} />
+        <Section description={"Guardian Request Handling"} onClick={() => onOpenApprovalRequests(true)} title={"Approve Requests"} logo={<ApprovalIcon htmlColor="var(--color-text-primary)" style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}} />} />
+        <Section description={"Guardian Handling Records"} onClick={() => onOpenHistory(true)} title={"View History"} logo={<ViewHistoryIcon htmlColor="var(--color-text-primary)" style={{width: "var(--svg-size-cover)", height: "var(--svg-size-cover)"}} />} />
+      </YoStyled>
+      {/* <Yo /> */}
+    </>
+    
     // return (
     //   <WrongStatus
     //     content={"The connected wallet is a Loopring Smart Wallet. Please use your Loopring Wallet mobile app to add Guardians."}
